90dc903a1e2b759067221fd14d7a182d49ed02f0
Trade Mashriq <trade.mashriq@gmail.com>
2018-05-16 22:25:52 +0000

    base files modified

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
 const express = require ('express');
+const path = require('path');
 const bodyParser = require("body-parser");
-const rc1 = require('./routes/rc1');
+const cors = require('cors');
+const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', ()=> {
+    console.log('Connected to Database ' + config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err)=> {
+    console.log('Databse connection error: '+err);
+});
+
 const app = express();
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// Set routes
+const rc1 = require('./routes/rc1');
+const users = require('./routes/users');
 
 const port = process.env.PORT || 8080;
 const ip = process.env.IP;
 
-app.use("/test", (req, res)=>{
-    res.send("Hello World");
-});
+// Use middleware for Cross Origin Reference*
+app.use(cors());
 
+
+// Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static folder
+app.use(express.static('public')); 
+//app.use(express.static(path.join(_dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+// routes
 app.use('/rc1', rc1);
+app.use('/users', users);
+
+//Index Route
+
+app.get('/', (req, res)=>{
+    res.send('Invalid Endpoint');
+})
 
 app.listen(port, ip,  ()=>{
     console.log("Server Started on Port "+ port + " at IP " + ip);
